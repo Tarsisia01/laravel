82c576b94d508852e911ddbda05cbdb25b78f95e
tarsisia <160700956+tarsisia@users.noreply.github.com>
2024-03-20 21:02:12 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/testing', function () {
+    return view('testing');
+});
+Route::get('/biodata', function () {
+    return view('biodata');
+});
+Route::get('/halamanproduk', function () {
+    return view('halamanproduk');
+});
+Route::get('/bukutamu', function () {
+    return view('bukutamu');
+});
+
